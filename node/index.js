6ff05924b70b001dca2c7690d7ed3ab79d8fcea4
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2019-02-16 14:59:16 -0200

    integrando front com back(parte 1) via ajax

---
+++ node/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();         
+const bodyParser = require('body-parser');
+const port = 3000; //porta padrão
+const sql = require('mssql');
+const connStr = "Server=XXX;Database=XXX;User Id=XXX;Password=XXX;";
