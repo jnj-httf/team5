ecfb33b2853b22940ebed44b498e90b3e8c3d890
Paulo Camargo <grigt@uol.com.br>
2019-02-16 13:11:13 -0200

    Adiciona PullCity.ps1 para retornar cidade

---
+++ PullCity.ps1
@@ -0,0 +1,24 @@
+﻿param (
+    [string]$city
+)
+[string]$path = "$env:TEMP\ubs.json"
+
+#Build cache file if one does not exist
+if ((Test-Path -Path $path)) {
+    $ubs = Get-Content -Path $path | ConvertFrom-Json
+}
+else {
+    [int]$pages = (Invoke-RestMethod -Uri "http://api-ldc-hackathon.herokuapp.com/api/ubs/" | Select-Object -ExpandProperty _metadata | Select-Object -ExpandProperty page) -split "/" | Select-Object -Last 1
+
+    Invoke-Command {
+         for ($i = 1; $i -le $pages; $i++) {
+            $ubs = Invoke-RestMethod -Uri "http://api-ldc-hackathon.herokuapp.com/api/ubs/$i/"
+            $ubs.records
+         }
+    } | ConvertTo-Json | Out-File -Encoding utf8 $path
+}
+
+if ($city) {
+    #Return city found
+    $ubs | Where-Object { $_.dsc_cidade -imatch "$city*" } | ConvertTo-Html -Property dsc_cidade, nom_estab, dsc_endereco, dsc_bairro
+}
