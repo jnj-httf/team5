fe0af91547e90e8a841be4e535a1db73430b620d
Rafael Celegato <rafaelcelegato@hotmail.com>
2019-02-16 15:08:22 -0200

    Repository, Services, Entity/Model/Code-First com Migration Enabled, Database with loaded data from webService and partially implemented front-end

---
+++ JnJHackathon_Desafio1/JnJHackathon_Desafio1/Repository/JnJHackathonDbContext.cs
@@ -0,0 +1,14 @@
+﻿using JnJHackathon_Desafio1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JnJHackathon_Desafio1.Repository
+{
+    public class JnJHackathonDbContext : DbContext
+    {
+        public DbSet<UBS> UBSs { get; set; }
+    }
+}
