0daf132e2daae2bd94504d120a47cbd68e02ff43
Enry Goncalves <enry_lucas@hotmail.com>
2019-02-16 10:43:23 -0200

    big json

---
+++ PullData.ps1
@@ -0,0 +1,8 @@
+﻿[int]$pages = (Invoke-RestMethod -Uri "http://api-ldc-hackathon.herokuapp.com/api/ubs/" | Select-Object -ExpandProperty _metadata | Select-Object -ExpandProperty page) -split "/" | Select-Object -Last 1
+
+Invoke-Command {
+     for ($i = 1; $i -le $pages; $i++) {
+        $ubs = Invoke-RestMethod -Uri "http://api-ldc-hackathon.herokuapp.com/api/ubs/$i/"
+        $ubs.records 
+     }
+} | Sort-Object -Property dsc_cidade | ConvertTo-Html -Property dsc_cidade, nom_estab, dsc_endereco, dsc_bairro | Out-File -FilePath "C:\Users\enry_\Desktop\Hackaton JnJ\team5\city-ubs.html"
---
+++ PullJson.ps1
@@ -0,0 +1,1 @@
+﻿Invoke-WebRequest -Uri http://api-ldc-hackathon.herokuapp.com/api/ubs/#/ | Select-Object -ExpandProperty Content | Out-File -Encoding utf8 "C:\Users\enry_\Desktop\Hackaton JnJ\team5\sample.json"
